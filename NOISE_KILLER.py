98bcf1b156b6aebc3a6fed98974b6fb9a1bc53e1
jromaris <jromaris@itba.edu.ar>
2020-02-01 17:38:25 -0300

    Cambios boludos a terminate noise

--- NOISE_KILLER.py
+++ NOISE_KILLER.py
@@ -25,18 +25,6 @@ def read_audio(filename):
     print("Frecuencia de muestreo",fs)
     return input_audio,fs
 
-##Leo el audio con ruido agregado
-#wav = wavio.read("el_gato.wav")
-#input_audio=wav.data[:,0]
-#input_audio = input_audio/np.max(np.abs(input_audio),axis=0)
-#fs=wav.rate
-#print("Frecuencia de muestreo",fs)
-
-#wav = wavio.read("los_sinso_solos.wav")
-#original=wav.data[:,0]
-#original = original/np.max(np.abs(original),axis=0)
-#fs=wav.rate
-
 def cancel_noise(filename):
 
     #Leo el archivo de audio deseado
@@ -50,10 +38,10 @@ def cancel_noise(filename):
     
     #En caso de ser necesario agrego zero padding
     if (win_num*(WINDOW_SIZE/2)+WINDOW_SIZE/2) < len(input_audio):
-      audio=zeros(len(input_audio))
+        audio=zeros(len(input_audio))
     
     else:  
-      audio=zeros(int(win_num*(WINDOW_SIZE/2)+WINDOW_SIZE/2))
+        audio=zeros(int(win_num*(WINDOW_SIZE/2)+WINDOW_SIZE/2))
     
     audio[0:len(input_audio)]=input_audio
     
@@ -68,28 +56,31 @@ def cancel_noise(filename):
     
     for i in range(int(win_num)):
     
-      #Aplico la ventana
-      curr_win=audio[int(WINDOW_SIZE/2)*i:int(WINDOW_SIZE/2)*(i+2)]*hanning
+        #Aplico la ventana
+        curr_win=audio[int(WINDOW_SIZE/2)*i:int(WINDOW_SIZE/2)*(i+2)]*hanning
     
-      #Opero
-      curr_win,noise_mean=terminate_noise(curr_win,i,noise_mean,WINDOW_SIZE,10,8)
+        #Opero
+        curr_win,noise_mean=terminate_noise(curr_win,i,noise_mean,WINDOW_SIZE,10,8)
     
-      #Calculo la salida
-      output[i*int(WINDOW_SIZE/2):(i+1)*int(WINDOW_SIZE/2)]=curr_win[0:int(WINDOW_SIZE/2)]+temp_win
+        #Calculo la salida
+        output[i*int(WINDOW_SIZE/2):(i+1)*int(WINDOW_SIZE/2)]=curr_win[0:int(WINDOW_SIZE/2)]+temp_win
       
-      #Actualizo la ventana temporal
-      temp_win=curr_win[int(WINDOW_SIZE/2)-1:-1]
+        #Actualizo la ventana temporal
+        temp_win=curr_win[int(WINDOW_SIZE/2)-1:-1]
     
     freqs=np.linspace(0,fs*(WINDOW_SIZE/2-1)/WINDOW_SIZE,WINDOW_SIZE/2+1)
     
     #Grafico de la estimación espectral del ruido estacionario
+    """
     print("Grafico amplitud de la estimación espectral del ruido")
     plt.plot(freqs,noise_mean**2)
     plt.xlabel("Frecuencia [Hz] ")
     plt.ylabel("Energía del ruido")
     plt.grid(True)
     plt.show()
-    
+    """
+
+    """
     print("Grafico ambos audios con ruido y audio filtrado")
     #Grafico ambos audios con ruido y audio filtrado
     plt.figure(figsize=(15,10))
@@ -99,10 +90,11 @@ def cancel_noise(filename):
     plt.grid(True)
     plt.legend(loc='lower left')
     plt.show()
-    
+    """
+
     #Escribo el wav del audio filtrado
     wavio.write("salida_filtrada.wav", output, fs, sampwidth=3) 
-    return output, fs
+    return output, audio, fs
 
 
 cancel_noise(archivo)
